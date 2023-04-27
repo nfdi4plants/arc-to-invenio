a4391043c30e555a8396ed2522133450bec7f29e
Kevin Schneider <schneider.kev@outlook.de>
2023-04-27 09:19:32 +0200

    containerize CLI tool

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
-FROM mcr.microsoft.com/dotnet/sdk:7.0
+FROM mcr.microsoft.com/dotnet/sdk:6.0
 
-COPY ./invenioConverter.fsx /
+ENV ARC_PATH=/arc
+
+COPY ./ /opt/arc-to-invenio
+WORKDIR /opt/arc-to-invenio
+RUN chmod +x build.sh
+RUN ./build.sh runtests
+
+ENV PATH="${PATH}:/opt/arc-to-invenio/src/arc-to-invenio/bin/Release/net6.0"
+
+WORKDIR /arc
--- tests/ReferenceObjects.fs
+++ tests/ReferenceObjects.fs
@@ -3,9 +3,9 @@
 open System.IO
 
 module Programmatic =
-    let investigation_1_formatted = File.ReadAllText("fixtures/json/programmatic/investigation_1_formatted.json")
-    let investigation_1_unformatted = File.ReadAllText("fixtures/json/programmatic/investigation_1_unformatted.json")
+    let investigation_1_formatted = File.ReadAllText("fixtures/json/programmatic/investigation_1_formatted.json").ReplaceLineEndings()
+    let investigation_1_unformatted = File.ReadAllText("fixtures/json/programmatic/investigation_1_unformatted.json").ReplaceLineEndings()
 
 module IO = 
-    let investigation_1_formatted = File.ReadAllText("fixtures/json/io/investigation_1_formatted.json")
-    let investigation_1_unformatted = File.ReadAllText("fixtures/json/io/investigation_1_unformatted.json")
+    let investigation_1_formatted = File.ReadAllText("fixtures/json/io/investigation_1_formatted.json").ReplaceLineEndings()
+    let investigation_1_unformatted = File.ReadAllText("fixtures/json/io/investigation_1_unformatted.json").ReplaceLineEndings()
